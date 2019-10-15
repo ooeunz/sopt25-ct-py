8f168791b89db3b88abdc40335dd5e562ea9c43f
seunghee63 <0603yang@naver.com>
2019-10-16 00:24:12 +0900

    Remove stack class

--- 10799.py
+++ 10799.py
@@ -1,32 +1,19 @@
-class stack_class:
-    def __init__(self):
-        self.__stack_list = list()
-    
-    def push(self, value):       
-        self.__stack_list.append(value)
-
-    def pop(self):
-        self.__stack_list.pop()
-
-    def size(self):
-        return len(self.__stack_list)
-
 if __name__ == "__main__":
-    stack = stack_class()
-    total_cnt = 0
+stack = list()
+total_cnt = 0
 
-    input_str = input()
-    temp_pre = ''
+input_str = input()
+temp_pre = ''
 
-    for each in input_str:
-        if each is '(':
-            stack.push(each)
-        elif each is ')' and temp_pre is '(':
-            stack.pop()
-            total_cnt += stack.size()
-        elif each is ')':
-            stack.pop()
-            total_cnt += 1
-        temp_pre = each
-    
-    print(total_cnt)
+for each in input_str:
+    if each is '(':
+        stack.append(each)
+    elif each is ')' and temp_pre is '(':
+        stack.pop()
+        total_cnt += len(stack)
+    elif each is ')':
+        stack.pop()
+        total_cnt += 1
+    temp_pre = each
+
+print(total_cnt)
--- 9012.py
+++ 9012.py
@@ -1,16 +1,3 @@
-class stack_class:
-    def __init__(self):
-        self.__stack_list = list()
-    
-    def push(self, value):       
-        self.__stack_list.append(value)
-
-    def pop(self):       
-        self.__stack_list.pop()
-
-    def size(self):
-        return len(self.__stack_list)
-
 def isVPS(size):
     if size == 0 :
         print("YES")
@@ -22,19 +9,19 @@ if __name__ == "__main__":
     input_cnt = input()
 
     for i in range(0,int(input_cnt),1):
-        stack = stack_class()
+        stack = list()
         input_str = input()
-        stack_size = stack.size()   
+        stack_size = len(stack)
 
         for each in input_str:
             if each is '(':
-                stack.push(each)
-            elif each is ')' and stack.size() == 0:
+                stack.append(each)
+            elif each is ')' and len(stack) == 0:
                 stack_size = -1
                 break
             elif each is ')':
                 stack.pop()
             
-            stack_size = stack.size()
+            stack_size = len(stack)
 
         isVPS(stack_size)
