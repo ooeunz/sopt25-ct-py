092570f7c160db98cc594783fb11f9e2156ebb2b
tape22 <gjwjdlas@gmail.com>
2019-12-11 18:03:17 +0900

    [정민]ㅇ

--- week9/9372.py
+++ week9/9372.py
@@ -1,8 +1,9 @@
 T = int(input()) # 테스트 케이스 수
-case =[]
+res =[]
+
 for _ in range(T):
     N, M = list(map(int,input().split()))
     for _ in range(M):
         a,b = list(map(int,input().split()))
-    case.append(N-1)
-print(*case,sep='\n')
+    res.append(N-1)
+print(*res,sep='\n')
